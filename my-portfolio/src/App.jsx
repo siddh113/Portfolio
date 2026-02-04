18b52439798e4da2522be3f736354e3057417ec9
Siddharth <siddharthketan@gmail.com>
2026-02-04 02:07:49 -0500

    router changes

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster"
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
 import { pagesConfig } from './pages.config'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import PageNotFound from './lib/PageNotFound';
 
 const { Pages, Layout, mainPage } = pagesConfig;
