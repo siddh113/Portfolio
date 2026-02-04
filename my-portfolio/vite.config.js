96b7c725d7008efc6416e8831e018d36e17876fc
Siddharth <siddharthketan@gmail.com>
2026-02-04 01:58:04 -0500

    gh pages dependencies

--- my-portfolio/vite.config.js
+++ my-portfolio/vite.config.js
@@ -5,6 +5,7 @@ import { fileURLToPath, URL } from 'node:url'
 // https://vite.dev/config/
 export default defineConfig({
   logLevel: 'error', // Suppress warnings, only show errors
+  base: '/Portfolio/',
   plugins: [
     react(),
   ],
